ad736e05e9e18505e5e69f170e1b3efd3ab16717
joseaguerrar <joseaguerrar2017@gmail.com>
2024-07-10 19:01:33 -0600

    Ya están más pruebas para un coverage de 100%

--- tests/tokenizer_test.cc
+++ tests/tokenizer_test.cc
@@ -45,5 +45,78 @@ TEST(TokenizerTest, DecimalGrande) {
     EXPECT_EQ(numero->valor(), 2.005);
 }
 
+TEST(TokenizerTest, TokenizeNumeros) {
+    Tokenizer tokenizer("123 456");
+    Token* token1 = tokenizer.Next();
+    TokenNumero* numToken1 = dynamic_cast<TokenNumero*>(token1);
+    ASSERT_NE(numToken1, nullptr);
+    EXPECT_EQ(numToken1->valor(), 123);
+
+    Token* token2 = tokenizer.Next();
+    TokenNumero* numToken2 = dynamic_cast<TokenNumero*>(token2);
+    ASSERT_NE(numToken2, nullptr);
+    EXPECT_EQ(numToken2->valor(), 456);
+
+    EXPECT_FALSE(tokenizer.HasNext());
+}
+
+TEST(TokenizerTest, TokenizeOperadorSum) {
+    Tokenizer tokenizer("+");
+    Token* token = tokenizer.Next();
+    TokenOperador* opToken = dynamic_cast<TokenOperador*>(token);
+    ASSERT_NE(opToken, nullptr);
+    EXPECT_EQ(opToken->operador(), '+');
+    EXPECT_FALSE(tokenizer.HasNext());
+}
+
+TEST(TokenizerTest, TokenizeOperadorMenos) {
+    Tokenizer tokenizer("-");
+    Token* token = tokenizer.Next();
+    TokenOperador* opToken = dynamic_cast<TokenOperador*>(token);
+    ASSERT_NE(opToken, nullptr);
+    EXPECT_EQ(opToken->operador(), '-');
+    EXPECT_FALSE(tokenizer.HasNext());
+}
+
+TEST(TokenizerTest, TokenizeOperadorMult) {
+    Tokenizer tokenizer("*");
+    Token* token = tokenizer.Next();
+    TokenOperador* opToken = dynamic_cast<TokenOperador*>(token);
+    ASSERT_NE(opToken, nullptr);
+    EXPECT_EQ(opToken->operador(), '*');
+    EXPECT_FALSE(tokenizer.HasNext());
+}
+
+TEST(TokenizerTest, TokenizeOperadorDiv) {
+    Tokenizer tokenizer("/");
+    Token* token = tokenizer.Next();
+    TokenOperador* opToken = dynamic_cast<TokenOperador*>(token);
+    ASSERT_NE(opToken, nullptr);
+    EXPECT_EQ(opToken->operador(), '/');
+    EXPECT_FALSE(tokenizer.HasNext());
+}
+
+TEST(TokenizerTest, TokenizeParenIzq) {
+    Tokenizer tokenizer("(");
+    Token* token = tokenizer.Next();
+    TokenOperador* opToken = dynamic_cast<TokenOperador*>(token);
+    ASSERT_NE(opToken, nullptr);
+    EXPECT_TRUE(opToken->IsLeftParenthesis());
+    EXPECT_FALSE(tokenizer.HasNext());
+}
+
+TEST(TokenizerTest, TokenizeParenDer) {
+    Tokenizer tokenizer(")");
+    Token* token = tokenizer.Next();
+    TokenOperador* opToken = dynamic_cast<TokenOperador*>(token);
+    ASSERT_NE(opToken, nullptr);
+    EXPECT_TRUE(opToken->IsRightParenthesis());
+    EXPECT_FALSE(tokenizer.HasNext());
+}
+
+TEST(TokenizerTest, Numeroinvalido) {
+    Tokenizer tokenizer("123.45.67");
+    EXPECT_THROW(tokenizer.Next(), std::runtime_error);
+}
 
 
